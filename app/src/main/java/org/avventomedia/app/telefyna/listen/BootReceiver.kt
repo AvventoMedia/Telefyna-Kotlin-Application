8b2dacf12c4e253198a5afb2b1e277d453836f18
Mutagubya Jonathan <mutajonathan4@gmail.com>
2026-01-19 20:10:33 +0300

    Revert "📱 Introduce Foreground PlayerService with MediaController & MVVM Arch…"
    This reverts commit 744036c2057035876fe18b6e4997a1ca2cee0d94.

--- app/src/main/java/org/avventomedia/app/telefyna/listen/BootReceiver.kt
+++ app/src/main/java/org/avventomedia/app/telefyna/listen/BootReceiver.kt
@@ -10,11 +10,7 @@ class BootReceiver : BroadcastReceiver() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            // Start playback service to resume 24/7 playback
-            val startIntent = Intent(context, org.avventomedia.app.telefyna.service.PlayerService::class.java).apply {
-                action = org.avventomedia.app.telefyna.service.PlayerService.ACTION_START
-            }
-            context.startForegroundService(startIntent)
+            Maintenance().scheduleNextMaintenance()
         }
     }
 }
